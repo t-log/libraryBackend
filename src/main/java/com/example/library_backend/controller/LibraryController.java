d50e199b9c52dae8b62789c2900b79165970e772
akhil <akhil.paul@ymail.com>
2022-12-05 20:37:23 +0530

    Add library page routing

--- src/main/java/com/example/library_backend/controller/LibraryController.java
+++ src/main/java/com/example/library_backend/controller/LibraryController.java
@@ -1,9 +1,17 @@
 package com.example.library_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LibraryController {
+
+    @GetMapping("/")
+    public String homeLibraryPage(){return "Home Library Page";}
+
+    @PostMapping("/add")
+    public String addLibraryPage(){return "Add Library Page";}
 
 
 
